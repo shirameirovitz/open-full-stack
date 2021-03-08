f133d9de897c99331955db3a4fdcce30b10190ca
Shira-Meirovitz <77247378+shirameirovitz@users.noreply.github.com>
2021-03-08 19:56:13 +0200

    added a POST for a new person

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
+
 let phoneBook = [
     {
       id: 1,
@@ -22,15 +24,15 @@ let phoneBook = [
       number: "0508946244",
     },
   ];
-
+//GET to the main page
   app.get('/api/persons', (request, response) => {
     response.json(phoneBook);
   })
-
+//GET to info
 app.get("/info", (request, response) => {
   response.send(`PhoneBook has info for ${phoneBook.length} people <br> ${new Date()}`)
 })
-
+//GET for ID
 app.get("/api/persons/:id", (request, response) => {
 const id = Number(request.params.id);
 const person = phoneBook.find(person => person.id === id);
@@ -40,13 +42,46 @@ response.json(person);
   response.status(404).end()
 }
 })
-
+//DELETE by ID
 app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
    phoneBook = phoneBook.filter(person => person.id !== id)
 
    response.status(204).end()
 })
+
+//POST a new person
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  const genarateId = () => {
+  const maxId = phoneBook.length > 0 
+  ? Math.max(...phoneBook.map(n => n.id))
+  : 0
+  return maxId + 1
+  }
+
+if(!body) {
+  return response.status(400).json({error: "content missing"})
+}
+if(!body.name) {
+  return response.status(400).json({error: "name missing"})
+}
+if(!body.number) {
+  return response.status(400).json({error: "number missing"})
+}
+const person = {
+  id: generateId(),
+  name: body.name,
+  number: body.number, 
+}
+phoneBook = phoneBook.concat(person)
+console.log(generateId())
+response.json(phoneBook)
+})
+
+
+
 
 
   const PORT = 3001
