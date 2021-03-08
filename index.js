36be218e9f810312556753075a43616a975c2944
Shira-Meirovitz <77247378+shirameirovitz@users.noreply.github.com>
2021-03-08 17:44:56 +0200

    added get for id

--- index.js
+++ index.js
@@ -31,7 +31,16 @@ app.get("/info", (request, response) => {
   response.send(`PhoneBook has info for ${phoneBook.length} people <br> ${new Date()}`)
 })
 
+app.get("/api/persons/:id", (request, response) => {
+const id = Number(request.params.id);
+const person = phoneBook.find(person => person.id === id);
+if (person) {
+response.json(person);
+} else {
+  response.status(404).end()
+}
 
+})
 
   const PORT = 3001
   app.listen(PORT)
